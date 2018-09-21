7217ba2c6f0420928c2f546f38624c86af259b9e
Carson McLean <camclean@ualberta.ca>
2018-09-21 18:54:43 -0400

    Classifier is running

--- homework/hw1/hw1_code.py
+++ homework/hw1/hw1_code.py
@@ -1,20 +1,46 @@
+
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+import pandas
 
 
-def load_data():
-    fake_file = open("clean_fake.txt", "r")
-    real_file = open("clean_real.txt", "r")
+fake_file = open("clean_fake.txt", "r")
+real_file = open("clean_real.txt", "r")
 
-    fake_data = fake_file.read().splitlines()
-    real_data = real_file.read().splitlines()
+fake_data = fake_file.read().splitlines()
+real_data = real_file.read().splitlines()
 
-    print(fake_data)
+titles = []
+labels = []
 
-    vectorizer = TfidfVectorizer()
-    fake_vecs = vectorizer.fit_transform(fake_data)
-    real_vecs = vectorizer.fit_transform(real_data)
+for title in fake_data:
+    titles.append(title)
+    labels.append("fake")
 
-    print(fake_vecs.shape)
-    print(real_vecs.shape)
+for title in real_data:
+    titles.append(title)
+    labels.append("real")
 
-load_data()
+df = pandas.DataFrame()
+df["titles"] = titles
+df["labels"] = labels
+
+print(df.head())
+
+train_titles, test_titles, train_labels, test_labels = train_test_split(df["titles"], df["labels"])
+
+
+vectorizer = TfidfVectorizer()
+vectorizer.fit(df["titles"])
+train_vecs = vectorizer.transform(train_titles)
+test_vecs = vectorizer.transform(test_titles)
+
+classifier = DecisionTreeClassifier()
+classifier.fit(train_vecs, train_labels)
+predictions = classifier.predict(test_vecs)
+
+print(accuracy_score(predictions, test_labels))
