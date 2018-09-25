2950f51578c07d2eaea164d775e23be8dcd84f28
Carson McLean <camclean@ualberta.ca>
2018-09-25 02:02:59 -0400

    Removes old code and visualizes the tree

--- homework/hw1/hw1_code.py
+++ homework/hw1/hw1_code.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.utils import shuffle
 
 import numpy as np
@@ -74,45 +74,10 @@ def select_model(datasets, vectorizer):
             print("\nMax Depth: " + str(max_depth) + " Split Criteria: " + str(split_criteria))
             print(accuracy_score(predictions, validation["label"]))
 
-
-
-def old():
-    fake_file = open("clean_fake.txt", "r")
-    real_file = open("clean_real.txt", "r")
-
-    fake_data = fake_file.read().splitlines()
-    real_data = real_file.read().splitlines()
-
-    titles = []
-    labels = []
-
-    for title in fake_data:
-        titles.append(title)
-        labels.append("fake")
-
-    for title in real_data:
-        titles.append(title)
-        labels.append("real")
-
-    df = pandas.DataFrame()
-    df["titles"] = titles
-    df["labels"] = labels
-
-    print(df.head())
-
-    train_titles, test_titles, train_labels, test_labels = train_test_split(df["titles"], df["labels"])
-
-
-    vectorizer = TfidfVectorizer()
-    vectorizer.fit(df["titles"])
-    train_vecs = vectorizer.transform(train_titles)
-    test_vecs = vectorizer.transform(test_titles)
-
-    classifier = DecisionTreeClassifier()
-    classifier.fit(train_vecs, train_labels)
-    predictions = classifier.predict(test_vecs)
-
-    print(accuracy_score(predictions, test_labels))
+            export_graphviz(
+                decision_tree=classifier,
+                out_file=split_criteria+str(max_depth),
+            )
 
 def pr(level, input):
     if PRINT_LEVEL >= level:
