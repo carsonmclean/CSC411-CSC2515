d02430a33a143fdad2b55e3b98200db3f59098f5
Carson McLean <camclean@ualberta.ca>
2018-09-25 01:20:29 -0400

    Implements Vectorizer and splits into data sets

--- homework/hw1/hw1_code.py
+++ homework/hw1/hw1_code.py
@@ -4,9 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.utils import shuffle
 
 import numpy as np
 import pandas
+import math
 
 def load_data():
     fake_file = open("clean_fake.txt", "r")
@@ -21,6 +23,27 @@ def load_data():
     print("fake.shape()" + str(fake_df.shape))
     print("real.shape()" + str(real_df.shape))
     print("data.shape()" + str(df.shape))
+
+    # ekad, tj89 -> https://stackoverflow.com/questions/29576430/shuffle-dataframe-rows
+    df = shuffle(df)
+    print("data.head()" + str(df.head()))
+
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(df['titles'])
+
+    count = df.shape[0]
+    train = df[0:math.floor(count*0.7)]
+    validation = df[math.floor(count*0.7):math.floor(count*0.85)]
+    test = df[math.floor(count*0.85):]
+    print("train: " + str(train.shape))
+    print("validation: " + str(validation.shape))
+    print("test: " + str(test.shape))
+    print("\n% real labelled data in each set:")
+    print(train[train["label"] == "real"].count()["label"]/train.shape[0])
+    print(validation[validation["label"] == "real"].count()["label"]/validation.shape[0])
+    print(test[test["label"] == "real"].count()["label"]/test.shape[0])
+    print("\n% real labelled data overall:")
+    print(len(real_data)/(len(fake_data)+len(real_data)))
 
 def old():
     fake_file = open("clean_fake.txt", "r")
