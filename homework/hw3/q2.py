71f26dc8e3949a51d0d930b86f606d40e1a2f868
Carson McLean <camclean@ualberta.ca>
2018-10-11 01:17:33 -0400

    Update to newest starter code

--- homework/hw3/q2.py
+++ homework/hw3/q2.py
@@ -7,35 +7,36 @@ from __future__ import print_function
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_boston
+
 np.random.seed(0)
 
 # load boston housing prices dataset
 boston = load_boston()
 x = boston['data']
 N = x.shape[0]
-x = np.concatenate((np.ones((506,1)),x),axis=1) #add constant one feature - no bias needed
+x = np.concatenate((np.ones((506, 1)), x), axis=1)  # add constant one feature - no bias needed
 d = x.shape[1]
 y = boston['target']
 
 idx = np.random.permutation(range(N))
 
-#helper function
-def l2(A,B):
+
+# helper function
+def l2(A, B):
     '''
     Input: A is a Nxd matrix
            B is a Mxd matirx
     Output: dist is a NxM matrix where dist[i,j] is the square norm between A[i,:] and B[j,:]
     i.e. dist[i,j] = ||A[i,:]-B[j,:]||^2
     '''
-    A_norm = (A**2).sum(axis=1).reshape(A.shape[0],1)
-    B_norm = (B**2).sum(axis=1).reshape(1,B.shape[0])
-    dist = A_norm+B_norm-2*A.dot(B.transpose())
+    A_norm = (A ** 2).sum(axis=1).reshape(A.shape[0], 1)
+    B_norm = (B ** 2).sum(axis=1).reshape(1, B.shape[0])
+    dist = A_norm + B_norm - 2 * A.dot(B.transpose())
     return dist
 
- 
- 
-#to implement
-def LRLS(test_datum,x_train,y_train, tau,lam=1e-5):
+
+# to implement
+def LRLS(test_datum, x_train, y_train, tau, lam=1e-5):
     '''
     Input: test_datum is a dx1 test vector
            x_train is the N_train x d design matrix
@@ -49,12 +50,10 @@ def LRLS(test_datum,x_train,y_train, tau,lam=1e-5):
     ## TODO
 
 
-
-
-def run_validation(x,y,taus,val_frac):
+def run_validation(x, y, taus, val_frac):
     '''
     Input: x is the N x d design matrix
-           y is the N x 1 targets vector    
+           y is the N x 1 targets vector
            taus is a vector of tau values to evaluate
            val_frac is the fraction of examples to use as validation data
     output is
@@ -68,8 +67,7 @@ def run_validation(x,y,taus,val_frac):
 
 if __name__ == "__main__":
     # In this excersice we fixed lambda (hard coded to 1e-5) and only set tau value. Feel free to play with lambda as well if you wish
-    taus = np.logspace(1.0,3,200)
-    train_losses, test_losses = run_validation(x,y,taus,frac_val=0.3)
+    taus = np.logspace(1.0, 3, 200)
+    train_losses, test_losses = run_validation(x, y, taus, val_frac=0.3)
     plt.semilogx(train_losses)
     plt.semilogx(test_losses)
-
