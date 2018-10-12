1e366796775d82fea8a273d66ea57bb6a2a55c17
Carson McLean <camclean@ualberta.ca>
2018-10-12 12:25:36 -0400

    Plotting code and image for huber loss

---
+++ homework/hw3/q1a.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = np.arange(-7, 7.5, 0.5)
+
+delta = 2
+huber = np.piecewise(x,
+                    [abs(x) <= delta, abs(x) > delta],
+                    [lambda x: 1/2*x**2, lambda x: delta*(abs(x) - delta/2)])
+plt.plot(x, huber, label="Huber Loss (δ=2)")
+plt.plot(x, (1/2)*x**2, label="Squared Error Loss")
+plt.xlabel("(y-t)")
+plt.ylabel("L(y,t)")
+plt.legend()
+plt.savefig("q1a_plot.png")
+plt.show()
